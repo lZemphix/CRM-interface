7d3eb2e8908e043d2ad4cd625ccb4163225a965e
Zemphix <vafinaaa51@gmail.com>
2026-09-15 23:53:50 +0500

    feat: add CRM design foundation

---
+++ lib/core/theme/light/colorscheme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const sidebarBackground = Color(0xFF101828);
+  static const activeElement = Color(0xFF5B5BD6);
+  static const hoveredElement = Color(0xFF232a39);
+  static const sidebarElement = Color(0xFF8B93A0);
+  static const notActiveBorder = Color(0xFFE4E7EC);
+}
